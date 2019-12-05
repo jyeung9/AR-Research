418df36ca179b0cee61842b071fb0c4b27af2ff1
jyeung9 <44103932+jyeung9@users.noreply.github.com>
2019-12-05 16:28:06 -0500

    start

---
+++ Pause.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    private float time = 0.0f;
+    public float waitTime;  // length of pause
+
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject aiCar = GameObject.FindWithTag("car");
+        var go = aiCar.GetComponent<UnityStandardAssets.Vehicles.Car.CarController>();
+        go.m_Topspeed = (float)0.0;
+        //print(time);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        time = time + Time.deltaTime;
+
+        if(time >= 5)
+        {
+            GameObject aiCar = GameObject.FindWithTag("car");
+            var go = aiCar.GetComponent<UnityStandardAssets.Vehicles.Car.CarController>();
+            go.m_Topspeed = (float) 25.0;
+        }
+    }
+}
