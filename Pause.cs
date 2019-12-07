aea03c594ed61650f0499f67eb38332f4d9d4a79
Priya Misner <priya.misner@tufts.edu>
2019-12-07 16:01:22 -0500

    fixed the pause script

--- Pause.cs
+++ Pause.cs
@@ -4,27 +4,49 @@ using UnityEngine;
 
 public class Pause : MonoBehaviour
 {
-    private float time = 0.0f;
-    public float waitTime;  // length of pause
 
-    // Start is called before the first frame update
+    public float pauseTime = 5;
+    bool entered = false;
+    Timer timer;
+   
+
+    void Start()
+    {
+        timer = gameObject.AddComponent<Timer>();
+        timer.Duration = pauseTime;
+        
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer.Finished)
+        {
+            GameObject aiCar = GameObject.FindWithTag("Car");
+            var go = aiCar.GetComponent<UnityStandardAssets.Vehicles.Car.CarController>();
+            go.m_Topspeed = 200;
+        }
+
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        GameObject aiCar = GameObject.FindWithTag("car");
-        var go = aiCar.GetComponent<UnityStandardAssets.Vehicles.Car.CarController>();
-        go.m_Topspeed = (float)0.0;
-        //print(time);
-    }
-
-    private void OnTriggerStay(Collider other)
-    {
-        time = time + Time.deltaTime;
-
-        if(time >= 5)
+        if (entered == false)
         {
-            GameObject aiCar = GameObject.FindWithTag("car");
+            GameObject aiCar = GameObject.FindWithTag("Car");
             var go = aiCar.GetComponent<UnityStandardAssets.Vehicles.Car.CarController>();
-            go.m_Topspeed = (float) 25.0;
+            go.m_Topspeed = (float)0.0;
+            timer.Run();
+            entered = true;
         }
     }
+
+    private void OnTriggerExit(Collider other)
+    {
+        entered = false;
+        
+
+    }
+
 }
